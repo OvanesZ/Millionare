b29793207e4834779c8c4a81709c4499ab4ba454
Ovanes <o-zakharyan@mail.ru>
2022-03-22 20:17:54 +0300

    create new project Millionare

---
+++ Millionare/Millionare/MillionareApp.swift
@@ -0,0 +1,17 @@
+//
+//  MillionareApp.swift
+//  Millionare
+//
+//  Created by Ованес Захарян on 22.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MillionareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
