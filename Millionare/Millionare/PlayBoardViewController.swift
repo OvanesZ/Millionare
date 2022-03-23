6d5b68a43b07fcff34774d9236deeee175b0b480
Ovanes <o-zakharyan@mail.ru>
2022-03-23 19:50:37 +0300

    add struct Question and 5 questions

---
+++ Millionare/Millionare/PlayBoardViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PlayBoardViewController.swift
+//  Millionare
+//
+//  Created by Ованес Захарян on 23.03.2022.
+//
+
+import UIKit
+
+
+
+class PlayBoardViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    private var questions = [Question(question: "Столица России?", answer: ["Москва", "Санкт-Петербург", "Казань", "Екатеринбург"], trueAnswer: "Москва"),
+                             Question(question: "Какой химический элемент назван в честь злого подземного гнома", answer: ["Гафний", "Кобальт", "Бериллий", "Теллур"], trueAnswer: "Кобальт"),
+                             Question(question: "Где у кузнечика уши?", answer: ["На спине", "На хвосте", "На ногах", "Их нет"], trueAnswer: "На ногах"),
+                             Question(question: "Какое дерево дольше живет?", answer: ["Сосна", "Осина", "Дуб", "Ель"], trueAnswer: "Дуб"),
+                             Question(question: "Как звали убийцу А.С. Пушкина?", answer: ["Жульен", "дядя Степа", "Мартынов", "Дантес"], trueAnswer: "Дантес")]
+    
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+        
+    }
+    
+}
---
+++ Millionare/Millionare/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  Millionare
+//
+//  Created by Ованес Захарян on 22.03.2022.
+//
+
+import UIKit
+
+
+struct Question {
+    let question: String
+    let answer: [String]
+    let trueAnswer: String
+}
+
+
+
