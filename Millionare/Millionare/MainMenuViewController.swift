7c5e469a43b344c3873b7836dd265e3a19d45eaa
Ovanes <o-zakharyan@mail.ru>
2022-03-23 23:06:48 +0300

    task 2. add mainMenu window and game window

---
+++ Millionare/Millionare/MainMenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainMenuViewController.swift
+//  Millionare
+//
+//  Created by Ованес Захарян on 23.03.2022.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+    
+    
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+}
