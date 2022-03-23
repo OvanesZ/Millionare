166b22dac9360c625c09a1782613164f4c740a52
Ovanes <o-zakharyan@mail.ru>
2022-03-23 21:29:20 +0300

    task 1

---
+++ Millionare/Millionare/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Millionare
+//
+//  Created by Ованес Захарян on 23.03.2022.
+//
+
+import UIKit
+
+struct Question {
+    let question: String
+    let answer: [String]
+    let trueAnswer: String
+}
